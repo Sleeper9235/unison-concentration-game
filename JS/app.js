670634d5a35aeb065bdf659165e8750b0995dd81
Sleeper9235 <kylejyoumans@gmail.com>
2024-06-08 16:03:09 -0500

    collapsed buttons to a forEach loop. Created 3x3 board. completed js for toggling visibilty for each board

--- JS/app.js
+++ JS/app.js
@@ -10,18 +10,42 @@ function changeColor() {
 myObjects.forEach(object => object.addEventListener('click', changeColor))
 myObjects.forEach(object => object.addEventListener('touch', changeColor))
 
-//grabs the 2x2 button
-const twoByTwoButton = document.querySelector('#practiceDifficulty')
+// grabs the 2x2 button (dead due to forEach loop through all difficulty buttons) (keeping for reference)
+
+// const twoByTwoButton = document.querySelector('#practiceDifficulty')
+
+//function for listener below
+// function amIHidden() {
+//     twoByTwoGrid.classList.toggle('iAmHiddenNow')
+// }
+
+//adds an event listener to the button
+// twoByTwoButton.addEventListener('click', amIHidden)
+
+
+//grabs all the buttons by class name
+const allTheButtons = document.querySelectorAll('.difficulty')
 //grabs the 2x2 grid section
 const twoByTwoGrid = document.querySelector('.twoByTwo')
+//grabs the 3x3 grid section
+const threeByThreeGrid = document.querySelector('.threeByThree')
 //function for listener below
 function amIHidden() {
-    if (twoByTwoGrid.classList) {
-        twoByTwoGrid.classList.toggle('iAmNow')
-        console.log(twoByTwoGrid)
-    }
-
+    if (this.id === "practiceDifficulty") {
+        twoByTwoGrid.classList.toggle('iAmHiddenNow')
+    } else if (this.id === "easyDifficulty"){
+        threeByThreeGrid.classList.toggle('iAmHiddenNow')
+    } else if (this.id === "mediumDifficulty") {
+        console.log('4')
+    } else if (this.id === "hardDifficulty") {
+        console.log('5')
+    }  
 }
-//adds an event listener to the button
-twoByTwoButton.addEventListener('click', amIHidden)
-//adds an event listener to the grid section
+//forEach loop to iterate through the buttons and add an event listener and a function to each.
+allTheButtons.forEach(button => button.addEventListener('click', amIHidden))
+allTheButtons.forEach(button => button.addEventListener('touch', amIHidden))
+
+//grabs the parentGrid section 
+const parentGrid = document.querySelector('#parentGrid')
+
+console.log(parentGrid)
