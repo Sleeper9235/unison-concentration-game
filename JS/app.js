2413ba6bb05a3c58ac5182c47afd27a890511e2c
Sleeper9235 <kylejyoumans@gmail.com>
2024-06-10 11:34:36 -0500

    Adjusted css/html/js for better functionality. Instruction buttow works. Requires checkBox to close

--- JS/app.js
+++ JS/app.js
@@ -1,35 +1,31 @@
 
+let userSelectionOne = {}
+let userSelectionTwo = {}
 
-//grabs each object in the section
-const myObjects = document.querySelectorAll('.eachObject')
-//function for listener below
-function changeColor() {
-    this.classList.toggle('change')
-}
-//loops through all the objects and for each objects adds event listeners
-myObjects.forEach(object => object.addEventListener('click', changeColor))
-myObjects.forEach(object => object.addEventListener('touch', changeColor))
-
-// grabs the 2x2 button (dead due to forEach loop through all difficulty buttons) (keeping for reference)
-
-// const twoByTwoButton = document.querySelector('#practiceDifficulty')
-
-//function for listener below
-// function amIHidden() {
-//     twoByTwoGrid.classList.toggle('iAmHiddenNow')
-// }
-
-//adds an event listener to the button
-// twoByTwoButton.addEventListener('click', amIHidden)
-
-
+//grabs the instruction dialog box
+const dialog = document.getElementById('instructionBox')
+//grabs the instruction button
+const instructionButton = document.querySelector('#instruction')
+//grab the close button in the instructions popup box
+const closeInstructionButton = document.getElementById('continueButton')
+//grabs the check box
+const checkBox = document.getElementById('instructionsCheckBox')
 //grabs all the buttons by class name
 const allTheButtons = document.querySelectorAll('.difficulty')
 //grabs the 2x2 grid section
 const twoByTwoGrid = document.querySelector('.twoByTwo')
 //grabs the 3x3 grid section
 const threeByThreeGrid = document.querySelector('.threeByThree')
-//function for listener below
+//grabs each object in the section
+const myObjects = document.querySelectorAll('.eachObject')
+
+
+//function to change object color and play sound
+function changeColor() {
+    this.classList.toggle('change')
+}
+
+//function for buttons to toggle visibility
 function amIHidden() {
     if (this.id === "practiceDifficulty") {
         twoByTwoGrid.classList.toggle('iAmHiddenNow')
@@ -41,8 +37,31 @@ function amIHidden() {
         console.log('5')
     }  
 }
+
+//function for instruction dialog box w/ instruction button
+function toggleInstructions() {
+    dialog.showModal();
+}
+
+//function to close the instructions. Function only runs of checkbox in dialog is selected 
+function closeInstructions() {
+    if(checkBox.checked === true) {
+        dialog.close();
+    } else {
+        return
+    }
+}
+
 //forEach loop to iterate through the buttons and add an event listener and a function to each.
 allTheButtons.forEach(button => button.addEventListener('click', amIHidden))
 allTheButtons.forEach(button => button.addEventListener('touch', amIHidden))
+//loops through all the objects and for each objects adds event listeners
+myObjects.forEach(object => object.addEventListener('click', changeColor))
+myObjects.forEach(object => object.addEventListener('touch', changeColor))
+//adds an event listener to the instructions button 
+instructionButton.addEventListener('click', toggleInstructions)
+instructionButton.addEventListener('touch', toggleInstructions)
+//adds an event listener to the close button 
+closeInstructionButton.addEventListener('click', closeInstructions)
+closeInstructionButton.addEventListener('touch', closeInstructions)
 
-//grabs the parentGrid section 
